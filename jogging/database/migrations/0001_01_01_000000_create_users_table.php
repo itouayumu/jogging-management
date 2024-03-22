bc4243a571134360979037ec3b0076916dd0e497
itou <a.itou.sys22@morijyobi.ac.jp>
2024-03-22 09:33:53 +0900

    データベースとその他もろもろの設定

--- jogging/database/migrations/0001_01_01_000000_create_users_table.php
+++ jogging/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('age');
+            $table->string('gender');
+            $table->integer('weight')->nullable(false)->change();
+            $table->integer('height')->nullable(false)->change();
             $table->rememberToken();
             $table->timestamps();
         });
