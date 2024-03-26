168aef20b40edcf0f9ea9d50d4aad15ba91a4e6a
akira <a.oono.sys23@morijyobi.sc.jp>
2024-03-26 10:56:52 +0900

    データ入力ページの作成とその他
    入力ページの作成と,joggingController.php,routesのweb.php,viewsのlayouts/user.phpに変更又は追記

--- jogging/routes/web.php
+++ jogging/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\joggingController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,8 +9,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('data_regist',[joggingController::class,'data_regist']);
+// Route::get('data_regist',[コントローラー名::class,'処理の名前']);
+// Route::get('image',[test::class,'image']);
+// Route::post('img',[test::class,'img']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
