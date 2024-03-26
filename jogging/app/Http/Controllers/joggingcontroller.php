32424470f40d9624ee2b2d97a6012f1854086862
Fujiwara <113399284+useruser0123@users.noreply.github.com>
2024-03-26 12:11:16 +0900

    チェック

--- jogging/app/Http/Controllers/joggingcontroller.php
+++ jogging/app/Http/Controllers/joggingcontroller.php
@@ -3,26 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
-class joggingcontroller extends Controller
+class joggingController extends Controller
 {
-    public function index()
-    {
-        return view('auth.login');
+    //
+    public function data_regist(){
+       return view('jogging.data_regist');
     }
-    public function top()
-    {
-        return view('joging.top');
-    }
-    public function sab()
-    {
-        return view('joging.sab');
-    }
-    public function data(Request $request)
-    {
-        $queryParameters = $request->query();
-        $items = DB :: table('data')->where('created_at',$queryParameters)->first();
-        return view('joging.sab',['data'=>$items]);
-    }
+
 }
