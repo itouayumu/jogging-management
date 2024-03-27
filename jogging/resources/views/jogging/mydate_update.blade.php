611011f38e8eb86636f953a715e58d0ef6d062ef
h.motodate <h.motodate.sys23@morijyobi.ac.jp>
2024-03-27 12:08:28 +0900

    mypage_updateページの更新など

--- jogging/resources/views/jogging/mydate_update.blade.php
+++ jogging/resources/views/jogging/mydate_update.blade.php
@@ -2,67 +2,56 @@
 
 @section('title', 'Index')
 @section('css')
-<link rel="stylesheet" href="{{ asset('/css/mypage_update.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/mypage.css') }}">
 
 @section('content')
-<h2>アカウント情報</h2>
-
-<form action="#" method="post">
-    <div class="account_all">
-
-       <div class="account_1">
-
-         <div> <p>アイコン</p> </div>
-        <div>    
-         <label>アカウント名</label>
-         <input type="text" name="name"> 
-        </div>
-        <div>
-         <label>ランキング登録名</label>
-         <input type="text" name="ranking_name">
-        </div>
-        <div>
-         <label>メールアドレス</label>
-         <input type="text" name="email">
-        </div>
-        <div>
-         <label>年齢</label>
-         <input type="number" name="age" >歳
-        </div>
-        <div>
-         <label>目標設定</label>
-          <div>
-            <input type="checkbox">
-            <label for="">ダイエット</label>
-
-            <input type="checkbox">
-            <label for="">健康維持</label>
-
-        </div>
-    </div>
-</div>
-<div class="account_2">
-    <div>
-        <label for="">体重</label>
-        <input type="number">kg
-    </div>
-    <div>
-        <label for="">身長</label>
-        <input type="number">cm
-    </div>
-
+<h1>アカウント情報</h1>
+<div class="container">
+    <div class="content1">
     
-<div class="button">
-<input type="button" value="編集完了">
-<input type="button" value="戻る">
-
+        <div class="box">
+            <h2>アイコン</h2>
+            <img src="" alt="Icon" width="100">
+        </div>
+        <div class="box">
+            <h2>アカウント名</h2>
+            <input type="text">
+        </div>
+        <div class="box">
+            <h2>メールアドレス</h2>
+            <div class="info-box">
+                <input type="text">
+            </div>
+        </div>
+        <div class="box">
+            <h2>年齢</h2>
+            <div class="info-box">
+                <input type="number">歳
+            </div>
+        </div>
+        </div>
+        <div class="content2">
+        <div class="box">
+            <h2>目標設定</h2>
+            <div class="info-box">
+                <input type="checkbox" name="diet">ダイエット
+                <input type="checkbox" name="health">健康維持
+            </div>
+        </div>
+        <div class="box">
+            <h2>体重</h2>
+            <div class="info-box">
+                <input type="number">kg
+            </div>
+        </div>
+        <div class="box">
+            <h2>身長</h2>
+            <div class="info-box">
+                <input type="number">cm
+            </div>
+        </div>
+        <button class="edit-button">編集完了</button>
+        <a href="mypage"><button class="top-button">戻る</button></a>
+    </div>
 </div>
-
-</div>
-
-
-</div>
-
-
-</form>
 @endsection
