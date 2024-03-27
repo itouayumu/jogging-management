eb883381636c6c16cb438688295f336d074610dd
akira <a.oono.sys23@morijyobi.sc.jp>
2024-03-27 09:35:21 +0900

    豆知識

--- jogging/routes/web.php
+++ jogging/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-
-
-use App\Http\Controllers\joggingController;
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\joggingcontroller;
 use App\Http\Controllers\mypage_update;
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 
 
@@ -19,15 +20,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('index',[joggingcontroller::class,'index']);
 Route::get('top',[joggingcontroller::class,'top']);
 Route::get('sab',[joggingcontroller::class,'sab']);
+Route::get('data_regist',[joggingcontroller::class,'data_regist']);
+Route::post('data_regist',[joggingcontroller::class,'data_regist_result']);
 Route::get('data',[joggingcontroller::class,'data']);
 
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('data_regist',[joggingController::class,'data_regist']);
+// Route::get('data_regist',[joggingController::class,'data_regist']);
 // Route::get('data_regist',[コントローラー名::class,'処理の名前']);
 // Route::get('image',[test::class,'image']);
 // Route::post('img',[test::class,'img']);
 
 Route::get('update',[mypage_update::class,'update']);
+
+Route::get('trivia',[joggingcontroller::class,'trivia']);
